13300d38b8f0b77722afadc29a20b4dc878f8868
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 22:46:26 +0530

    added

--- terraform/QA/eks_nodegroup.tf
+++ terraform/QA/eks_nodegroup.tf
@@ -5,7 +5,7 @@ module "eks_nodes" {
   subnet_private_us_east_1a = module.subnet.private_us_east_1a
   subnet_private_us_east_1b = module.subnet.private_us_east_1b
   node_group_name = "teraops-terraform-poc"
-  eks_node_group_instance_type = "t3.medium"
+  eks_node_group_instance_type = ["t3.medium"]
   node_desired_size = "2"
   node_max_size = "2"
   node_min_size = "1"
