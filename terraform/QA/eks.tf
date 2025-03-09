b5bec7cb47a8462e50f5b2ec200c0f02f7e8ef07
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 21:42:41 +0530

    added

--- terraform/QA/eks.tf
+++ terraform/QA/eks.tf
@@ -1,9 +1,9 @@
 module "eks" {
   source = "../modules/eks/eks_cluster"
   environment = var.environment
-  subnet_private_eu_west_2a = module.subnet.private_eu_west_2a
-  subnet_private_eu_west_2b = module.subnet.private_eu_west_2b
-  subnet_public_eu_west_2a = module.subnet.public_eu_west_2a
-  subnet_public_eu_west_2b = module.subnet.public_eu_west_2b
-  eks_cluster_name = var.eks_cluster_name
+  subnet_private_us_east_1a = module.subnet.private_us_east_1a
+  subnet_private_us_east_1b = module.subnet.private_us_east_1b
+  subnet_public_us_east_1a = module.subnet.public_us_east_1a
+  subnet_public_us_east_1b = module.subnet.public_us_east_1b
+  eks_cluster_name = "teraops-terraform-poc"
 }
