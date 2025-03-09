4eda99d8e6a7ad1c25fcef1fb7e9b1173f782e5c
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 17:15:22 +0530

    added

--- terraform/QA/subnets.tf
+++ terraform/QA/subnets.tf
@@ -1,10 +1,10 @@
 module "subnet" {
   source = "../modules/networking/subnet"
   vpc_id = module.vpc.vpc_id
-  subnet_private_1_availability_zone = "eu-west-2a"
-  subnet_private_2_availability_zone = "eu-west-2b"
-  subnet_public_1_availability_zone = "eu-west-2a"
-  subnet_public_2_availability_zone = "eu-west-2b"
+  subnet_private_1_availability_zone = "us-east-2a"
+  subnet_private_2_availability_zone = "us-east-2b"
+  subnet_public_1_availability_zone = "us-east-2a"
+  subnet_public_2_availability_zone = "us-east-2b"
   subnet_private-1 = "10.1.0.0/18"
   subnet_private-2 = "10.1.64.0/18"
   subnet_public-1 = "10.1.128.0/18"
--- terraform/modules/networking/subnet/output.tf
+++ terraform/modules/networking/subnet/output.tf
@@ -1,12 +1,12 @@
-output "private_eu_west_2a" {
-  value = aws_subnet.private_eu_west_2a.id
+output "private_us_east_2a" {
+  value = aws_subnet.private_us_east_2a.id
 }
-output "private_eu_west_2b" {
-  value = aws_subnet.private_eu_west_2b.id
+output "private_us_east_2b" {
+  value = aws_subnet.private_us_east_2b.id
 }
-output "public_eu_west_2a" {
-  value = aws_subnet.public_eu_west_2a.id
+output "public_us_east_2a" {
+  value = aws_subnet.public_us_east_2a.id
 }
-output "public_eu_west_2b" {
-  value = aws_subnet.public_eu_west_2b.id
+output "public_us_east_2b" {
+  value = aws_subnet.public_us_east_2b.id
 }
