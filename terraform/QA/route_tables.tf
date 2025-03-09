94083abe4fe497ee962d821f7d4a1ea2700ca767
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 18:05:00 +0530

    added

--- terraform/QA/route_tables.tf
+++ terraform/QA/route_tables.tf
@@ -1,9 +1,9 @@
 module "routes" {
   source = "../modules/networking/routes"
-  subnet_private_us-east_1a = module.subnet.private_us-east_1a
-  subnet_private_us-east_1b = module.subnet.private_us-east_1b
-  subnet_public_us-east_1a = module.subnet.public_us-east_1a
-  subnet_public_us-east_1b = module.subnet.public_us-east_1b
+  subnet_private_us_east_1a = module.subnet.private_us_east_1a
+  subnet_private_us_east_1b = module.subnet.private_us_east_1b
+  subnet_public_us_east_1a = module.subnet.public_us_east_1a
+  subnet_public_us_east_1b = module.subnet.public_us_east_1b
   vpc_id = module.vpc.vpc_id
   nat_id = module.nat.nat
   ig_id  = module.ig.ig
