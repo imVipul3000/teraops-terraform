62b3f79402fa068df1905837c917efb04168b501
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 18:09:55 +0530

    added

--- terraform/modules/networking/routes/main.tf
+++ terraform/modules/networking/routes/main.tf
@@ -2,22 +2,11 @@ resource "aws_route_table" "private" {
   vpc_id = var.vpc_id
 
   route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = var.nat_id
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-    },
-  ]
+  {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = var.nat_id
+  }
+]
 
   tags = {
     Name = "private-teraops-${var.environment}"
@@ -28,22 +17,11 @@ resource "aws_route_table" "public" {
   vpc_id = var.vpc_id
 
   route = [
-    {
-      cidr_block                 = "0.0.0.0/0"
-      gateway_id                 = var.ig_id
-      nat_gateway_id             = ""
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      vpc_peering_connection_id  = ""
-    },
-  ]
+  {
+    cidr_block  = "0.0.0.0/0"
+    gateway_id  = var.ig_id
+  }
+]
 
   tags = {
     Name = "public-teraops-${var.environment}"
