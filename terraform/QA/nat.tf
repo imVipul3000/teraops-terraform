6d67164d9d9396521f676a66bf6510230a273a34
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 17:42:42 +0530

    added

--- terraform/QA/nat.tf
+++ terraform/QA/nat.tf
@@ -1,5 +1,5 @@
 module "nat" {
-  source = "../../common/networking/nat"
+  source = "../modules/networking/nat"
   subnet_public_us_east_1a = module.subnet.public_us_east_1a
   environment = var.environment
 }
