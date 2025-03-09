bd5cc2dc3dfab8229e54035545d8c6f484ada4e7
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 18:11:54 +0530

    added

--- terraform/modules/networking/routes/main.tf
+++ terraform/modules/networking/routes/main.tf
@@ -2,11 +2,23 @@ resource "aws_route_table" "private" {
   vpc_id = var.vpc_id
 
   route = [
-  {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = var.nat_id
-  }
-]
+    {
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = var.nat_id
+      carrier_gateway_id         = null
+      core_network_arn           = null
+      destination_prefix_list_id = null
+      egress_only_gateway_id     = null
+      gateway_id                 = null
+      instance_id                = null
+      ipv6_cidr_block            = null
+      local_gateway_id           = null
+      network_interface_id       = null
+      transit_gateway_id         = null
+      vpc_endpoint_id            = null
+      vpc_peering_connection_id  = null
+    },
+  ]
 
   tags = {
     Name = "private-teraops-${var.environment}"
@@ -17,33 +29,25 @@ resource "aws_route_table" "public" {
   vpc_id = var.vpc_id
 
   route = [
-  {
-    cidr_block  = "0.0.0.0/0"
-    gateway_id  = var.ig_id
-  }
-]
+    {
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = var.ig_id
+      nat_gateway_id             = null
+      carrier_gateway_id         = null
+      core_network_arn           = null
+      destination_prefix_list_id = null
+      egress_only_gateway_id     = null
+      instance_id                = null
+      ipv6_cidr_block            = null
+      local_gateway_id           = null
+      network_interface_id       = null
+      transit_gateway_id         = null
+      vpc_endpoint_id            = null
+      vpc_peering_connection_id  = null
+    },
+  ]
 
   tags = {
     Name = "public-teraops-${var.environment}"
   }
-}
-
-resource "aws_route_table_association" "private-us-east-1a" {
-  subnet_id      = var.subnet_private_us_east_1a
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "private-us-east-1b" {
-  subnet_id      = var.subnet_private_us_east_1b
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "public-us-east-1a" {
-  subnet_id      = var.subnet_public_us_east_1a
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public-us-east-1b" {
-  subnet_id      = var.subnet_public_us_east_1b
-  route_table_id = aws_route_table.public.id
 }
