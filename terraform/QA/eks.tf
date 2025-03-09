b059f379a78242e957c4a27df3ba0ca4bb689542
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-09 21:32:51 +0530

    added

--- terraform/QA/eks.tf
+++ terraform/QA/eks.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source = "../terraform/modules/eks/eks_cluster"
+  source = "../modules/eks/eks_cluster"
   environment = var.environment
   subnet_private_eu_west_2a = module.subnet.private_eu_west_2a
   subnet_private_eu_west_2b = module.subnet.private_eu_west_2b
