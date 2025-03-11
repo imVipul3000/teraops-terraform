b87972e2c8fd99b86a4ef9828341969fb6b1e49f
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-11 21:40:32 +0530

    added artifact

--- terraform/QA/codeartifact.tf
+++ terraform/QA/codeartifact.tf
@@ -1,5 +1,5 @@
 module "codeartifact" {
-    source = "../modules/networking/codeartifact"
+    source = "../modules/codeartifact"
     domain = "teraops-poc-domain"
     repository = "teraops-poc-repository"
 }
