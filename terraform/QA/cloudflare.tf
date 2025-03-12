9bd70aa4128abd72ff88505b44c54f86e682828d
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-03-12 23:43:34 +0530

    added cloudflare

--- terraform/QA/cloudflare.tf
+++ terraform/QA/cloudflare.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "alb_dns" {
   zone_id = "9b4ae30f1b4b5845ba1ebad8decea1aa"  # Your Cloudflare zone ID
-  name    = "pocvipul"  # Subdomain (e.g., app.yourdomain.com)
-  value   = "k8s-default-teraopsp-bb5dbc417b-vipul.us-east-1.elb.amazonaws.com"  # Your ALB's DNS name
+  name    = "poc-terraform"  # Subdomain (e.g., app.yourdomain.com)
+  value   = "dummy-poc-endpoint-via-terraform"  # Your ALB's DNS name
   type    = "CNAME"
   ttl     = 1
   proxied = true  # Set to false if you want direct traffic
