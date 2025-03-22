3a6ca924c3cc39844b43db648d0213e783cc6887
Vipul Joshi <59138092+imVipul3000@users.noreply.github.com>
2025-03-22 20:34:56 +0530

    Update eks.tf

--- terraform/QA/eks.tf
+++ terraform/QA/eks.tf
@@ -5,5 +5,5 @@ module "eks" {
   subnet_private_us_east_1b = module.subnet.private_us_east_1b
   subnet_public_us_east_1a = module.subnet.public_us_east_1a
   subnet_public_us_east_1b = module.subnet.public_us_east_1b
-  eks_cluster_name = "teraops-terraform-poc"
+  eks_cluster_name = "teraops-terraform-pooc"
 }
