a2d5152fd5daba850935916ce3824c17ec8f95f0
Vipul Joshi <59138092+imVipul3000@users.noreply.github.com>
2025-04-03 21:52:38 +0530

    Update output.tf

--- terraform/modules/eks/eks_nodegroup/output.tf
+++ terraform/modules/eks/eks_nodegroup/output.tf
@@ -5,5 +5,5 @@ output "node_group_name" {
 
 output "node_role_arn" {
   description = "IAM role ARN of the EKS node group"
-  value       = aws_iam_role.eks_node_role.arn
+  value       = aws_iam_role.nodes.arn
 }
