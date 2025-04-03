49c572f191482108cc173a46b87a325e65ab9ef7
Vipul Joshi <59138092+imVipul3000@users.noreply.github.com>
2025-04-03 21:49:27 +0530

    Update output.tf

--- terraform/modules/eks/eks_nodegroup/output.tf
+++ terraform/modules/eks/eks_nodegroup/output.tf
@@ -2,3 +2,8 @@ output "node_group_name" {
   description = "The name of the EKS node group"
   value       = aws_eks_node_group.private-nodes.id
 }
+
+output "node_role_arn" {
+  description = "IAM role ARN of the EKS node group"
+  value       = aws_iam_role.eks_node_role.arn
+}
