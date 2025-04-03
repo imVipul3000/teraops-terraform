57a6ab33c2e94968ca299a6d154778e45218a44a
Vipul Joshi <59138092+imVipul3000@users.noreply.github.com>
2025-04-03 20:46:31 +0530

    Update provider.tf

--- terraform/QA/provider.tf
+++ terraform/QA/provider.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-poc-teraops-qa"
     key            = "staging-sandbox/terraform.tfstate"
-    region         = "us-west-1"
+    region         = "us-east-1"
     use_lockfile   = true
     encrypt        = true
   }
