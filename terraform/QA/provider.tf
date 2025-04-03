d4d32824a1d01159eeff1ff84de7e4fbd82e421c
Vipul Joshi <vipuljoshi@Tradeprints-MBP.lan>
2025-04-03 21:12:15 +0530

    added metrics

--- terraform/QA/provider.tf
+++ terraform/QA/provider.tf
@@ -1,26 +1,20 @@
 terraform {
-  required_version = "~> 1.11"
-
-  backend "s3" {
-    bucket       = "terraform-poc-teraops-qa"
-    key          = "staging-sandbox/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-
   required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.30"  # Adjust based on stability
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.7" # Ensure compatibility with your Terraform version
+      version = "~> 5.30"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.25" # Ensure compatibility
+      version = "~> 2.25"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.7"
     }
   }
 }
@@ -35,15 +29,8 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(aws_eks_cluster.EKS.certificate_authority[0].data)
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.EKS.endpoint
-    token                  = data.aws_eks_cluster_auth.cluster.token
-    cluster_ca_certificate = base64decode(aws_eks_cluster.EKS.certificate_authority[0].data)
-  }
-}
-
-# Fetch authentication details for EKS
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.EKS.name
+provider "kubectl" {
+  host                   = aws_eks_cluster.EKS.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(aws_eks_cluster.EKS.certificate_authority[0].data)
 }
